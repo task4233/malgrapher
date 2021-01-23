bd0ac72d0a7ceb28a1a6f2c09ce56def79319664
task4233 <tks.m1205@gmail.com>
2021-01-23 21:59:06 +0900

    add config files

---
+++ gdb/target/test.c
@@ -0,0 +1,19 @@
+int main() {
+  int flg=0, flg2=1;
+  if (flg) {
+    puts("TRUE");
+    if (flg2) {
+      puts("TT");
+    } else {
+      puts("TF");
+    }
+  } else {
+    puts("fALSE");
+    if (flg2) {
+      puts("FT");
+    } else {
+      puts("FF");
+    }
+  }
+  return 0;
+}
